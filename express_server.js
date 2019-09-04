951e19d5fa6e0c1244765026f1a6f33eac645444
Corah Goyette <corahgoyette@gmail.com>
2019-09-04 23:15:48 +0000

    new changes for day 3

--- express_server.js
+++ express_server.js
@@ -1,62 +1,73 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require ("cookie-parser")
-app.use(cookieParser())
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
-
-app.use(bodyParser.urlencoded({ extended: false })); 
 const cookieSession = require('cookie-session');
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+//==============================================================================
+// register the routes for my application
+//==============================================================================
+
+// register my home routes
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase, username : req.cookies.username};
+  res.render("urls_index", templateVars);
+});
+
+// process login route
+app.post("/login", (req, res) => {
+
+  // extract properties from the form body
+  let username = req.body.username;
+  
+  // set username into cookie
+  res.cookie("username", username);
+  res.redirect("/urls");
+});  
+
+//process logout route
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});  
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
-})
-
-app.get("/", (req, res) => {
-  res.render("index");
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("username", "hello");
-  res.render("/");
-})  
-
-app.use(cookieSession({
-  name: 'user_id',
-  keys: ['id']
-}));
-
-app.get("/", (req, res) => {
-  const templateVars = { user: undefined };
-  templateVars.user = req.session.user_id ? users[req.session.user_id] : undefined;
-  res.render('index', templateVars );
-});
-
-app.post("/login", (req, res) => {
-
-  for (let user in users) {
-    console.log(users[user]);
-    if (users[user].username === req.body.username) {
-      if (users[user].password === req.body.password) {
-        // res.cookie('user_id', user);
-        req.session.user_id = user;
-        break;
-      }
-    }
-  }
-  res.redirect('/');
-});
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
@@ -67,13 +78,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls/register", (req, res) => {
+  res.render("urls_register");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -95,10 +101,6 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);       
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("urls/login", (req, res) => {
 let templateVars = {
   username: req.cookies["username"] };
@@ -115,3 +117,34 @@ function generateRandomString() {
     }
   return randomString;
 }
+
+app.use(cookieSession({
+  name: 'user_id',
+  keys: ['id']
+}));
+
+app.get("/", (req, res) => {
+  const templateVars = { user: undefined };
+  templateVars.user = req.session.user_id ? users[req.session.user_id] : undefined;
+  res.render('index', templateVars );
+});
+
+app.post("/login", (req, res) => {
+  for (let user in users) {
+    console.log(users[user]);
+    if (users[user].username === req.body.username) {
+      if (users[user].password === req.body.password) {
+        req.session.user_id = user;
+        break;
+      }
+    }
+  }
+  res.redirect('/');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+/* ** to find users - have to call the user object */
